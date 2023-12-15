ecc7801f027ed13443a943d2068254fc128caa65
13NOONE37 <oliwerklauze22@gmail.com>
2023-12-15 14:26:12 +0100

    day 10 part 1 done

--- Day10/Part1.py
+++ Day10/Part1.py
@@ -1,101 +1,77 @@
-file = open('test.txt','r')
+file = open('input.txt','r')
 lines = [list(line) for line in file.read().split('\n')]
-# print(lines)
 
 WIDTH = len(lines[0])
 HEIGHT = len(lines)
 
+NORTH_OPTIONS=['|', 'F','7']
+SOUTH_OPTIONS=['|', 'L','J']
+WEST_OPTIONS=['-', 'F','L']
+EAST_OPTIONS=['-', 'J','7']
 
 def get_pipe(x,y):
-    if 0<x<WIDTH-1 or 0<y<HEIGHT-1:
-        return lines[x][y] 
+    if 0<=x<=WIDTH-1 and 0<=y<=HEIGHT-1:
+        return lines[y][x] 
     return None
 
-def getNextPipe(prev_x,prev_y,x,y,forcedPipe=None):      
-    top=get_pipe(x,y-1)
-    bottom=get_pipe(x,y+1)
-    left=get_pipe(x-1,y)
-    right=get_pipe(x+1,y)
+def getNextPipe(prev_x,prev_y,x,y):   
+    north=get_pipe(x,y-1)
+    south=get_pipe(x,y+1)
+    west=get_pipe(x-1,y)
+    east=get_pipe(x+1,y)
     current = get_pipe(x,y)
-    if forcedPipe:
-        current=forcedPipe
-
-    if not current: return None
 
     if current=='|':
-        if prev_y<=y and bottom:
-            #we are coming from north
-            #south solutions
-            if bottom in ('|', 'L', 'J'):
+        if prev_y<y and south:
+            if south in SOUTH_OPTIONS:
                 return x,y+1
-        if prev_y>=y and top:
-            #we are coming from south
-            #north solutions
-            if top in ('|', 'F', '7'):
+        if prev_y>y and north:
+            if north in NORTH_OPTIONS:
                 return x,y-1
             
     if current=='-':
-        if prev_x<=x and right:
-            #we are coming from west
-            #east solutions
-            if right in ('-', '7', 'J'):
+        if prev_x<x and east:
+            if east in EAST_OPTIONS:
                 return x+1,y
-        if prev_x>=x and left:
-            #we are coming from east
-            #west solutions
-            if left in ('-', 'F', 'L'):
+        if prev_x>x and west:
+            if west in WEST_OPTIONS:
                 return x-1,y
             
     if current=='L':
-        if prev_y<=y and right:
-            #We are coming from north
-            #east solutions
-            if top in ('-', 'J', '7'):
+        if prev_y<y and east:
+            if east in EAST_OPTIONS:
                 return x+1,y
-        if prev_x>=x and top:
-            #We are coming from east
-            #north solutions
-            if top in ('|', 'F', '7'):
+        if prev_x>x and north:
+            if north in NORTH_OPTIONS:
                 return x,y-1
             
     if current=='J':
-        if prev_y<=y and left:
-            #We are coming from north
-            #west solutions
-            if top in ('-', 'F', 'L'):
+        if prev_y<y and west:
+            if west in WEST_OPTIONS:
                 return x-1,y
-        if prev_x>=x and top:
-            #We are coming from west
-            #north solutions
-            if top in ('|', 'F', '7'):
+        if prev_x<x and north:
+            if north in NORTH_OPTIONS:
                 return x,y-1
             
     if current=='7':
-        if prev_y>=y and left:
-            #We are coming from south
-            #west solutions
-            if left in ('-', 'L', 'F'):
+        if prev_y>y and west:
+            if west in WEST_OPTIONS:
                 return x-1,y
-        if prev_x<=x and bottom:
-            #We are coming from west
-            #south solutions
-            if bottom in ('|', 'J', 'L'):
-                return x,y-1
+        if prev_x<x and south:
+            if south in SOUTH_OPTIONS:
+                return x,y+1
             
     if current=='F':
-        if prev_y>=y and right:
-            #We are coming from south
-            #east solutions
-            if right in ('-', '7', 'J'):
+        if prev_y>y and east:
+            if east in EAST_OPTIONS:
                 return x+1,y
-        if prev_x>=x and bottom:
-            #We are coming from east
-            #south solutions
-            if bottom in ('|', 'J', 'L'):
+        if prev_x>x and south:
+            if south in SOUTH_OPTIONS:
                 return x,y+1
     
     return None
-    
+
+
 START_POS = [0,0]
 POS = [0,0]
 PREV_POS = [0,0]
@@ -105,30 +81,57 @@ for i in range(0,HEIGHT):
         x = lines[i].index('S')
         y = i
 
-        #!!Replace  S with coresponding pipe type
-        #!!Tempoary solution
-        # print(x,y)
-        # print(getNextPipe(x,y,x,y,'|'))
-        # getNextPipe(x,y)
-        lines[x][y]='F'
-        START_POS[0]=0
-        START_POS[1]=2
-        PREV_POS[0]=0
-        PREV_POS[1]=2
-        POS[0]=1
-        POS[1]=2
-        # START_POS[0] = PREV_POS[0] = POS[0] = x
-        # START_POS[1] = PREV_POS[1] = POS[1] = y
-        break;
+        START_POS[0] =  x
+        START_POS[1] =  y
+        north=get_pipe(x,y-1)
+        south=get_pipe(x,y+1)
+        west=get_pipe(x-1,y)
+        east=get_pipe(x+1,y)
 
+        if north in NORTH_OPTIONS and south in SOUTH_OPTIONS:
+            lines[x][y]='|'
+            PREV_POS[0] = POS[0]=x
+            PREV_POS[1]=y-1
+            POS[1] = y+1
 
-#replace char with coresponding
+        elif west in WEST_OPTIONS and east in EAST_OPTIONS:
+            lines[x][y]='-'
+            PREV_POS[1] = POS[1]=y
+            PREV_POS[0]=x-1
+            POS[0] = x+1
 
-#update POS_UP and POS_DOWN
+        elif west in WEST_OPTIONS and south in SOUTH_OPTIONS:
+            lines[x][y]='7'
+            PREV_POS[0]=x-1
+            PREV_POS[1]=y
+            POS[0] = x
+            POS[1] = y+1
+
+        elif east in EAST_OPTIONS and south in SOUTH_OPTIONS:
+            lines[x][y]='F'
+            PREV_POS[0]=x+1
+            PREV_POS[1]=y
+            POS[0] = x
+            POS[1] = y+1
+
+        elif west in WEST_OPTIONS and north in NORTH_OPTIONS:
+            lines[x][y]='J'
+            PREV_POS[0]=x-1
+            PREV_POS[1]=y
+            POS[0] = x
+            POS[1] = y-1
+        
+        elif east in EAST_OPTIONS and north in NORTH_OPTIONS:
+            lines[x][y]='L'
+            PREV_POS[0]=x+1
+            PREV_POS[1]=y
+            POS[0] = x
+            POS[1] = y-1
+       
+
 index=0
 while START_POS!=POS:
     NEW_POS = getNextPipe(PREV_POS[0],PREV_POS[1],POS[0],POS[1])
-    print(NEW_POS)
     if NEW_POS:
         PREV_POS[0]=POS[0]
         PREV_POS[1]=POS[1]
@@ -142,6 +145,7 @@ while START_POS!=POS:
     
 
 
-
+index//=2
+index+=1
 print(f'Farthest position: {index}')
 file.close()
